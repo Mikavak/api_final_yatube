80edef2bbd54a6a909bc9ef81e3f6cf3591261ec
Mik <awvvak@gmail.com>
2024-06-10 20:30:28 +0300

    1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.authtoken import views
+from rest_framework.routers import SimpleRouter
+
+from .views import GroupList, LightCommentViewSet, PostList
+
+router_v1 = SimpleRouter()
+router_v1.register('posts', PostList)
+#router_v1.register('groups', GroupList)
+#router_v1.register(
+    r'posts/(?P<post_id>[\d]+)/comments', LightCommentViewSet,
+    basename='comments')
 
 urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,35 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
+
+from posts.models import Comment, Group, Post
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class LightCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly, permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get("post_id")
+        get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post_id=post_id)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get("post_id")
+        new_queryset = Comment.objects.filter(post=post_id)
+        return new_queryset
+
+
+class PostList(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly, permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupList(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
