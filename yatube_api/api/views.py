0d5e9f206f3dd27bb5cf1954dea588862c0fc7e2
Mik <awvvak@gmail.com>
2024-06-15 15:45:44 +0300

    4

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, generics, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from posts.models import Comment, Group, Post
+from posts.models import Group, Post
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
